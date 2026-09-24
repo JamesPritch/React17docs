0c2eaebf182efa7d8bd3527027e67692fd6c390a
James Pritchard <147166089+JamesPritch@users.noreply.github.com>
2026-09-24 14:57:03 +0100

    5.4 data flows down

--- 5.State_and_Lifecycle/app.js
+++ 5.State_and_Lifecycle/app.js
@@ -115,3 +115,48 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+
+// 5.4 Data flows down
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount () {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount () {
+    clearInterval(this.timerID);
+  }
+  tick () {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1>
+        <p> Good morning, the time is {this.state.date.toLocaleTimeString()}. </p>
+      </div>
+    );
+  }
+}
+
+function App () {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
